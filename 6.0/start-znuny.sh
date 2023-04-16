6e2de38bc289c8b42ecbfad76b6804e3f23edac6
Andre Sartori <dev@aph.dev.br>
2023-04-15 23:17:05 -0300

    script

--- 6.0/start-znuny.sh
+++ 6.0/start-znuny.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Change data connection
-sed -i 's/\(.*{DatabaseHost}.*\)127.0.0.1/\1'"${MYSQL_HOSTNAME}"'/' /opt/otrs/Kernel/Config.pm
-sed -i 's/\(.*{Database}.*\)otrs/\1'"otrs_${CUSTOMER_ID}"'/' /opt/otrs/Kernel/Config.pm
-sed -i 's/\(.*{DatabaseUser}.*\)otrs/\1'"${MYSQL_USERNAME}"'/' /opt/otrs/Kernel/Config.pm
-sed -i 's/\(.*{DatabasePw}.*\)some-pass/\1'"${MYSQL_PASSWORD}"'/' /opt/otrs/Kernel/Config.pm
+sed -i 's/\(.*{DatabaseHost}.*\)127.0.0.1/\1'"${DATABASE_HOST}"'/' /opt/otrs/Kernel/Config.pm
+sed -i 's/\(.*{Database}.*\)otrs/\1'"otrs_${DATABASE}"'/' /opt/otrs/Kernel/Config.pm
+sed -i 's/\(.*{DatabaseUser}.*\)otrs/\1'"${DB_USER}"'/' /opt/otrs/Kernel/Config.pm
+sed -i 's/\(.*{DatabasePw}.*\)some-pass/\1'"${DB_PASS}"'/' /opt/otrs/Kernel/Config.pm
 
 # Start Apache
 service apache2 start
