60507567ab21ea5fe57c0c8c653c42f3cde600be
Andre Sartori <dev@aph.dev.br>
2023-04-15 22:42:47 -0300

    Adjust start script

--- 6.0/start-znuny.sh
+++ 6.0/start-znuny.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-if [ ! -f "/opt/otrs/installed" ]  ; then
-
-    # Change data connection
-    sed -i 's/\(.*{DatabaseHost}.*\)127.0.0.1/\1'"${MYSQL_HOSTNAME}"'/' /opt/otrs/Kernel/Config.pm
-    sed -i 's/\(.*{Database}.*\)otrs/\1'"otrs_${CUSTOMER_ID}"'/' /opt/otrs/Kernel/Config.pm
-    sed -i 's/\(.*{DatabaseUser}.*\)otrs/\1'"${MYSQL_USERNAME}"'/' /opt/otrs/Kernel/Config.pm
-    sed -i 's/\(.*{DatabasePw}.*\)some-pass/\1'"${MYSQL_PASSWORD}"'/' /opt/otrs/Kernel/Config.pm
-
-    # Test if mysql running
-    while ! mysqladmin ping -h "${MYSQL_HOSTNAME}" -u "${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" -u"${MYSQL_USERNAME}" --silent; do sleep 1; echo "Wainting Mysql...\n"; done
-
-    # Create database
-    mysql -h "${MYSQL_HOSTNAME}" -u "${MYSQL_USERNAME}" -p"${MYSQL_PASSWORD}" -e "CREATE DATABASE otrs_${CUSTOMER_ID} DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;" && \
-
-fi
-
-touch "/opt/otrs/installed"
+# Change data connection
+sed -i 's/\(.*{DatabaseHost}.*\)127.0.0.1/\1'"${MYSQL_HOSTNAME}"'/' /opt/otrs/Kernel/Config.pm
+sed -i 's/\(.*{Database}.*\)otrs/\1'"otrs_${CUSTOMER_ID}"'/' /opt/otrs/Kernel/Config.pm
+sed -i 's/\(.*{DatabaseUser}.*\)otrs/\1'"${MYSQL_USERNAME}"'/' /opt/otrs/Kernel/Config.pm
+sed -i 's/\(.*{DatabasePw}.*\)some-pass/\1'"${MYSQL_PASSWORD}"'/' /opt/otrs/Kernel/Config.pm
 
 # Start Apache
 service apache2 start
