fd6507d53f8feb78dd7a204b6b5bd5f8a1329d6a
Andre Sartori <dev@aph.dev.br>
2023-04-16 16:46:15 -0300

    Script modify

--- 6.0/start-znuny.sh
+++ 6.0/start-znuny.sh
@@ -2,15 +2,14 @@
 
 # Change data connection
 sed -i 's/\(.*{DatabaseHost}.*\)127.0.0.1/\1'"${DATABASE_HOST}"'/' /opt/otrs/Kernel/Config.pm
-sed -i 's/\(.*{Database}.*\)otrs/\1'"otrs_${DATABASE}"'/' /opt/otrs/Kernel/Config.pm
+sed -i 's/\(.*{Database}.*\)otrs/\1'"${DATABASE}"'/' /opt/otrs/Kernel/Config.pm
 sed -i 's/\(.*{DatabaseUser}.*\)otrs/\1'"${DB_USER}"'/' /opt/otrs/Kernel/Config.pm
 sed -i 's/\(.*{DatabasePw}.*\)some-pass/\1'"${DB_PASS}"'/' /opt/otrs/Kernel/Config.pm
 
-# Start Apache
-service apache2 start
-
 # Start cron
+su -c "/opt/otrs/bin/Cron.sh start" -s /bin/bash otrs
 service cron start
 
-# Start OTRS Daemon
-su -c "/opt/otrs/bin/Cron.sh start" -s /bin/bash otrs
+# Start Apache
+apachectl -D FOREGROUND
+
