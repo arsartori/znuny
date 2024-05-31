81c8d9be9aad59fe67bb369e528ed1f9eb0972e2
André Sartori <49533777+arsartori@users.noreply.github.com>
2024-05-31 19:09:26 -0300

    Update start-znuny.sh

--- start-znuny.sh
+++ start-znuny.sh
@@ -2,5 +2,7 @@
 # Start cron
 su -c "/opt/otrs/bin/Cron.sh start" -s /bin/bash otrs
 service cron start
+# Set Permission on all files
+/opt/otrs/bin/otrs.SetPermissions.pl
 # Start Apache
 apachectl -D FOREGROUND
