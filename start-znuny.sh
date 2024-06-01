76cf1ab1fa97a089dc6a76be77bbeb4d5a20a1de
arsartori <arsartori@outlook.com>
2024-06-01 16:09:50 +0000

    Update start script

--- start-znuny.sh
+++ start-znuny.sh
@@ -4,5 +4,7 @@ su -c "/opt/otrs/bin/Cron.sh start" -s /bin/bash otrs
 service cron start
 # Set Permission on all files
 /opt/otrs/bin/otrs.SetPermissions.pl
+# Update Database
+su -c "/opt/otrs/scripts/DBUpdate-to-6.pl" -s /bin/bash otrs
 # Start Apache
 apachectl -D FOREGROUND
