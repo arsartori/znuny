83a6f2cc24ba87776adb593cb6205ac2f61ab592
André Sartori <49533777+arsartori@users.noreply.github.com>
2024-06-02 20:05:47 -0300

    Update start-znuny.sh

--- 6.0/start-znuny.sh
+++ 6.0/start-znuny.sh
@@ -2,13 +2,10 @@
 
 # Set Permission on all files
 /opt/otrs/bin/otrs.SetPermissions.pl
-
 # Update Database
 su -c "/opt/otrs/scripts/DBUpdate-to-6.pl" -s /bin/bash otrs
-
 # Start cron
 su -c "/opt/otrs/bin/Cron.sh start" -s /bin/bash otrs
 service cron start
-
 # Start Apache
 apachectl -D FOREGROUND
