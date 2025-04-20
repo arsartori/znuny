b4cad0b812dfff41674c2069ae3e845eef002ab6
André Sartori <arsartori@outlook.com>
2025-04-20 18:42:32 -0300

    Criando diretórios

---
+++ v7.0/start-znuny.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Set Permission on all files
+/opt/otrs/bin/otrs.SetPermissions.pl
+# Update Database
+if [ $ZNUNY_UPDATE == 'yes' ]; then
+        echo "Opção Update selecionada. Atualizando..."
+	su -c "/opt/otrs/scripts/DBUpdate-to-6.pl" -s /bin/bash otrs
+elif [ $ZNUNY_UPGRADE == 'yes' ]; then
+        echo "Opção Upgrade selecionada. Atualizando..."
+	su -c "/opt/otrs/scripts/MigrateToZnuny6_5.pl" -s /bin/bash otrs
+else
+	echo "Nenhuma opção selecionada"
+fi
+# Start cron
+su -c "/opt/otrs/bin/Cron.sh start" -s /bin/bash otrs
+service cron start
+# Start Apache
+apachectl -D FOREGROUND
