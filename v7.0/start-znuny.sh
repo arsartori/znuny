146d3a73d9f6dd0ed8a7ba4ae6965a8dc6790e46
André Sartori <49533777+arsartori@users.noreply.github.com>
2025-04-20 19:41:04 -0300

    Atualizado

--- v7.0/start-znuny.sh
+++ v7.0/start-znuny.sh
@@ -4,7 +4,7 @@
 # Update Database
 if [ $ZNUNY_UPGRADE == 'yes' ]; then
         echo "Atualizando ..."
-	su -c "/opt/znuny/scripts/MigrateToZnuny7_0.pl" -s /bin/bash otrs
+	su -c "/opt/znuny/scripts/MigrateToZnuny7_0.pl" -s /bin/bash znuny
 fi
 # Start cron
 su -c "/opt/znuny/bin/Cron.sh start" -s /bin/bash otrs
